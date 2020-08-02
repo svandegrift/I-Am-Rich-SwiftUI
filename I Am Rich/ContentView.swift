f45555daebb17ab5849f4a1c8c2f9741ecd76943
Steven Vandegrift <svandegrift@icloud.com>
2020-08-02 12:13:26 -0500

    My First SwiftUI App

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+            }
+        }
     }
 }
 
